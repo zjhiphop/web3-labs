26270d3e2719417fda3fba968fbfa417003962c1
jade.zhang <zjhiphop@gmail.com>
2022-10-14 09:39:25 +0800

    html fund: listen for transaction finish

--- html-fundme-fcc/index.js
+++ html-fundme-fcc/index.js
@@ -15,7 +15,6 @@ $fundBtn.onclick = async function fund() {
         // singer / wallet / with some gas
 
         // contract we interact with 
-
         // ^ ABI & Address
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         const singer = provider.getSigner()
@@ -26,14 +25,31 @@ $fundBtn.onclick = async function fund() {
             const transactionResponse = await contract.fund({
                 value: ethers.utils.parseEther(ethAmount)
             })
+
+            // listen for tx to be mined
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done")
         } catch (err) {
             console.log(err);
         }
-
-
-
     }
 }
+
+// transaction mine
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log("Mine transaction: " + transactionResponse.hash);
+
+    return new Promise((resolve, reject) => {
+
+        // listen for transaction to finish
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log("Mine transaction finished: " + transactionReceipt.confirmations);
+            resolve()
+        })
+    })
+
+}
+
 
 // withdraw
 $withdrawBtn.onclick = async function withdraw() {
