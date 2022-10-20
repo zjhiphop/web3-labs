43b3535e5e0bbdd100e2d91b53a556d6fc14d2ac
jade.zhang <zjhiphop@gmail.com>
2022-10-20 09:47:26 +0800

    wip hh lottery: request random words for winners

--- hardhat-lottery/contracts/Raffle.sol
+++ hardhat-lottery/contracts/Raffle.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 // Raffle
 
@@ -19,12 +20,29 @@ error Raffle_NotEnoughETHEntered();
 contract Raffle is VRFConsumerBaseV2{
     uint256 private immutable i_entranceFee; 
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATION = 2;
+    uint32 private constant NUM_WORDS = 2;
     
     // EVENTS
     event RaffleEnter(address);
+    event RequestedWaffleWinner(uint256);
+
      
-    constructor(address vrfCordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCordinatorV2) {
+    constructor(
+        address vrfCordinatorV2, uint256 entranceFee, 
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+        ) VRFConsumerBaseV2(vrfCordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit  = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -52,9 +70,10 @@ contract Raffle is VRFConsumerBaseV2{
 
     function requestRandomeNumber( ) external /* external is cheper than internal */ {
         // request random number
-
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(i_gasLane, i_subscriptionId, 
+        REQUEST_CONFIRMATION, i_callbackGasLimit, NUM_WORDS);
         // Once get it, start to process transactios
 
-
+        emit RequestedWaffleWinner(requestId);
     }
 }
