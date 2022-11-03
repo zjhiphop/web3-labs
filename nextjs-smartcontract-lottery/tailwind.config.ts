abdbf429191a92109cb30e6827c19e6f15b97e45
jade.zhang <zjhiphop@gmail.com>
2022-11-03 09:39:51 +0800

    feat: nextjs version lottery init

---
+++ nextjs-smartcontract-lottery/tailwind.config.ts
@@ -0,0 +1,9 @@
+const config = {
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+export default config
